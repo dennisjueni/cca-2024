0b43538dfe29de23faae7b8bde01e196f4f80cbe
Dennis Jüni <dennis.01@hispeed.ch>
2024-03-14 10:59:06 +0100

    Use new delete_cluster_cli function

--- scripts/delete.py
+++ scripts/delete.py
@@ -2,12 +2,11 @@ import click
 from scripts.utils import delete_cluster
 
 
-if __name__ == "__main__":
-    delete_cluster(debug=True)
-    print("########### Cluster deleted ###########")
-
-
 @click.command()
 @click.option("--debug", "-d", help="Debug Flag for deleting the cluster.", is_flag=True, default=False, type=bool)
 def delete_cluster_cli(debug: bool) -> None:
     delete_cluster(debug=debug)
+    print("########### Cluster deleted ###########")
+
+if __name__ == "__main__":
+    delete_cluster_cli()
