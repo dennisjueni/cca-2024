7f4114f10b11b210c3db960ce6c8dcec91e3fb2d
Alex Thillen <alexthillen852@gmail.com>
2024-04-28 14:43:10 +0200

    save guard

--- scripts/task3.py
+++ scripts/task3.py
@@ -5,7 +5,11 @@ import click
 import time
 import os
 
+from loguru import logger
+
 from scripts.utils import (
+    scp_command,
+    ssh_command,
     start_cluster,
     run_command,
     pods_ready,
@@ -13,7 +17,18 @@ from scripts.utils import (
     get_pods_info,
     copy_file_to_node,
     check_output,
+    env,
 )
+
+NODES = [
+    "client-agent-a",
+    "client-agent-b",
+    "client-measure",
+    "master-europe-west3-a",
+    "node-a-2core",
+    "node-b-4core",
+    "node-c-8core",
+]
 
 
 @click.command()
@@ -25,7 +40,11 @@ def task3(start: bool):
     NUM_ITERATIONS = 1
 
     if start:
-        start_cluster("part3.yaml", debug=DEBUG)  # TODO : Waits for cluster to be ready?
+        # setup cluster using kops and part3.yaml
+        env = start_cluster("part3.yaml")
+        start_memcached(env)
+        start_mcperf(env)
+        schedule_batch_jobs(env)
 
     # TODO : `memcached` and workload creation with pods (`kubectl`) instead of setting up cluster (`kops`) - modify yaml files
     #   - Start `memcached` pod (`kubectl create -f memcached.yaml`)
@@ -34,3 +53,40 @@ def task3(start: bool):
     #      - `make` on `client-agent-` & `client-measure` machines
     #      - ? something like : `gcloud compute ssh ... ubuntu@$(kubectl get nodes -o wide | grep client-agent | awk '{print $1}')`
     #   - After previous are running: Start pods with batch jobs (`kubectl create -f batch-job{j}.yaml` jor j in {blackscholes, ...})
+
+
+def start_memcached(env: dict) -> None:
+    # Start memcached pod using `kubectl create -f memcached.yaml`
+    # TODO : Set up 'part3/memcache-t1-cpuset.yaml'
+    pass
+
+
+def start_mcperf(env: dict) -> None:
+    # Start mcperf pod(s) using `kubectl create -f mcperf.yaml`
+    # use make_mcperf.sh script to build mcperf on client-agent and client-measure
+    d = {}
+    for line in get_node_info(d):
+        if line[0].startswith("client-agent-") or line[0].startswith("client-measure-"):
+            # copy make_mcperf.sh to the node
+            node = line[0]
+            logger.info(f"Copying mcperf files to {line[0]}")
+            res = scp_command(
+                "./install_mcperf.sh",  # local file
+                "~/install_mcperf.sh",  # remote file
+                node,
+            )
+            # run make_mcperf.sh on the node
+            res = ssh_command(node, "chmod +x ~/install_mcperf.sh && ~/install_mcperf.sh")
+            #
+            if node.startswith("client-agent-a"):
+                res = ssh_command(node, "./mcperf -T 2 -A")
+            elif node.startswith("client-agent-b"):
+                res = ssh_command(node, "./mcperf -T 4 -A")
+            elif node.startswith("client-measure"):
+                # TODO : select pod with memcached to get IP for pod "some-memcached"
+                MEMCACHED_IP = None  # TODO
+                res = ssh_command(node, f"./mcperf -s {MEMCACHED_IP} --loadonly")
+                res = ssh_command(node, f"./mcperf -s {MEMCACHED_IP} -a INTERNAL_AGENT_A_IP -a INTERNAL_AGENT_B_IP \
+                    --noload -T 6 -C 4 -D 4 -Q 1000 -c 4 -t 10 \
+                    --scan 30000:30500:5
+                    ")
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,18 @@
 import os
 import subprocess
 import sys
+from typing import Optional
+from loguru import logger
+
+#### SETUP ENVIRONMENT VARIABLES ########
 
 KOPS_STATE_STORE = "gs://cca-eth-2024-group-076-djueni/"
 PROJECT = "gcloud config get-value project"
+env = dict(os.environ.copy())
+env["KOPS_STATE_STORE"] = KOPS_STATE_STORE
+env["PROJECT"] = PROJECT
+env["ZONE"] = "europe-west3-a"
+env["USER"] = "ubuntu"
 
 # if os is not windows
 if os.name != "nt":
@@ -11,31 +20,65 @@ if os.name != "nt":
         "cca-eth" in subprocess.run("gcloud config get-value project".split(), capture_output=True).stdout.decode()
     ), "You are not in the correct project. Run 'gcloud config set project cca-eth-2024-group-076-djueni'"
 
+#### END SETUP ENVIRONMENT VARIABLES ####
 
-def run_command(command: list[str], env: dict[str, str] = dict(os.environ)) -> None:
+
+def run_command(command: list[str], env: dict[str, str] = env) -> subprocess.CompletedProcess[bytes]:
     res = subprocess.run(command, env=env, capture_output=True)
     if res.returncode != 0:
         print("\033[91m!!! ERROR OCCURED !!!\033[0m")  # Print in red
         print(res.stderr.decode("utf-8"))
 
     print(res.stdout.decode("utf-8"))
+    return res
 
+def scp_command(source_path: str, destination_path: str, node: str, env: dict[str, str] = env) -> subprocess.CompletedProcess[bytes]:
+    command = [
+        "gcloud",
+        "compute",
+        "scp",
+        "--ssh-key-file",
+        os.path.expanduser("~/.ssh/cloud-computing"),
+        "--zone",
+        env.get("ZONE", "europe-west3-a"),
+        source_path,
+        f"{env.get("USER", "ubuntu")}@{node}:{destination_path}",
+    ]
+    return run_command(command, env)
 
-def start_cluster(yaml_file: str, cluster_name: str) -> None:
+def ssh_command(node: str, command: str, env: dict[str, str] = env) -> subprocess.CompletedProcess[bytes]:
+    ssh_command = [
+        "gcloud",
+        "compute",
+        "ssh",
+        "--zone",
+        env.get("ZONE", "europe-west3-a"),
+        "--ssh-key-file",
+        os.path.expanduser("~/.ssh/cloud-computing"),
+        f"{env.get("USER", "ubuntu")}@{node}",
+        "--command",
+        command,
+    ]
+    return run_command(ssh_command, env)
+
+def start_cluster(yaml_file: str, cluster_name: str, env=env) -> None:
+    """
+    yaml_file: str - path to the yaml file that describes the cluster, e.g. "part3.yaml"
+    cluster_name: str - name of the cluster, e.g. "cca-eth-2024-group-076-djueni.k8s.local"
+    """
     print("########### Starting cluster ###########")
 
     # We create a copy of the current environment variables to make sure we don't modify the original
-    environment_dict = dict(os.environ)
 
-    # Update the environment variables
-    environment_dict["KOPS_STATE_STORE"] = KOPS_STATE_STORE
-    environment_dict["PROJECT"] = PROJECT
+    with open(yaml_file, "r") as f:
+        yaml = f.read()
+        assert cluster_name in yaml, f"Cluster name {cluster_name} not found in yaml file"
 
     create_bucket_command = ["gsutil", "mb", KOPS_STATE_STORE]
-    run_command(create_bucket_command, environment_dict)
+    run_command(create_bucket_command, env)
 
     create_command = ["kops", "create", "-f", yaml_file]
-    run_command(create_command, environment_dict)
+    run_command(create_command, env)
 
     create_secret_command = [
         "kops",
@@ -48,21 +91,24 @@ def start_cluster(yaml_file: str, cluster_name: str) -> None:
         "-i",
         os.path.expanduser("~/.ssh/cloud-computing.pub"),
     ]
-    run_command(create_secret_command, environment_dict)
+    run_command(create_secret_command, env)
 
     update_command = ["kops", "update", "cluster", "--name", cluster_name, "--yes", "--admin"]
-    run_command(update_command, environment_dict)
+    run_command(update_command, env)
 
+    logger.info("Waiting for cluster to be ready... (~10min)")
     validate_command = ["kops", "validate", "cluster", "--wait", "10m"]
-    run_command(validate_command, environment_dict)
+    run_command(validate_command, env)
 
+    logger.info("Cluster is ready.")
     view_command = ["kops", "get", "nodes", "-o", "wide"]
-    run_command(view_command, environment_dict)
+    run_command(view_command, env)
     print(
         "In order to ssh into one of the nodes, use:\n'gcloud compute ssh --ssh-key-file ~/.ssh/cloud-computing ubuntu@<MACHINE_NAME> --zone europe-west3-a'"
     )
 
     print("########### Cluster started ###########")
+    return env
 
 
 def delete_cluster(cluster_name: str) -> None:
@@ -97,15 +143,30 @@ def get_info(resource_type: str) -> list[list[str]]:
     return info
 
 
-def get_node_info() -> list[list[str]]:
-    return get_info("nodes")
+def get_node_info(d : Optional[dict]= None) -> list[list[str]]:
+    # Return: [ [node_name, status, roles, age, version, internal_ip, external_ip], ... ]
+    info = get_info("nodes")
+    if d is not None:
+        for line in info:
+            d[line[0][:-5]] = {
+                "node_name": line[0],
+                "status": line[1],
+                "roles": line[2],
+                "age": line[3],
+                "version": line[4],
+                "internal_ip": line[5],
+                "external_ip": line[6],
+            }
+    return info
 
 
 def get_pods_info() -> list[list[str]]:
+    # Return: [ [name, ready, status, restarts, age, ip, node], ... ]
     return get_info("pods")
 
 
 def get_jobs_info() -> list[list[str]]:
+    # Return: [ [name, completions, age], ... ]
     return get_info("jobs")
 
 
