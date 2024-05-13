6b740d5cd120e6a044463157eeaf28280431cb3a
Dennis Jüni <dennis.01@hispeed.ch>
2024-05-13 10:00:20 +0200

    update plots for 4.2

--- scripts/plot_task4.py
+++ scripts/plot_task4.py
@@ -16,7 +16,7 @@ for RUN in RUNS:
     file = open(os.path.join(BASE_DIR, f"int{INTERVAL}_run{RUN}/log.txt"), "r")
     lines = file.read().splitlines()
     MAX_LEN = 1000
-    runs = []  # (start, finish, name, machine, color, cores)
+    runs = []
 
     c_dict = {
         "parsec-blackscholes": "#CCA000",
@@ -26,9 +26,20 @@ for RUN in RUNS:
         "parsec-freqmine": "#0CCA00",
         "parsec-radix": "#00CCA0",
         "parsec-vips": "#CC0A00",
-        "memcached": "#f06eee",
-        "p95 latency": "royalblue",
-        "QPS": "orange",
+        "memcached": "#888888",
+        "p95 latency": "#6A0DAD",
+        "QPS": "#FFA500",
+    }
+
+    labels = {
+        "parsec-blackscholes": "Blackscholes",
+        "parsec-canneal": "Canneal",
+        "parsec-dedup": "Dedup",
+        "parsec-ferret": "Ferret",
+        "parsec-freqmine": "Freqmine",
+        "parsec-radix": "Radix",
+        "parsec-vips": "Vips",
+        "memcached": "Memcached",
     }
     start_events = {}
     start_times = []
@@ -83,7 +94,19 @@ for RUN in RUNS:
 
     MAX_LEN = (max(completion_times) - min(start_times)).total_seconds()
 
-    legend_elements = [Patch(facecolor=c_dict[i], label=i) for i in c_dict]
+    legend_elements = [
+        Patch(facecolor=c_dict[i], label=labels[i])
+        for i in [
+            "parsec-blackscholes",
+            "parsec-canneal",
+            "parsec-dedup",
+            "parsec-ferret",
+            "parsec-freqmine",
+            "parsec-radix",
+            "parsec-vips",
+            "memcached",
+        ]
+    ]
 
     fig, ax1 = plt.subplots(figsize=(20, 9))
     START = min(start_times)
@@ -106,7 +129,7 @@ for RUN in RUNS:
 
     count = 0
     max_p95 = 2000
-    max_q = 130_000
+    max_q = 105_000
     ax2 = ax1.twinx()
     xs = []
     qs = []
@@ -128,49 +151,62 @@ for RUN in RUNS:
         qs.append(float(qps))
         qs.append(float(qps))
         target = entry[-1]
-    ax1.step(xs, ps, color="royalblue")
-    ax2.step(xs, qs, color="orange")
+    ax1.step(xs, ps, color=c_dict["p95 latency"])
+    ax2.step(xs, qs, color=c_dict["QPS"])
+
+    ax1.axhline(y=1000, color="black", linestyle="--")
 
     HEIGHT = 100
-    HEIGHT_NO_OFFSET = 80
+    HEIGHT_NO_OFFSET = 90
     MARGIN = 100
     NODE_A_START = MARGIN + max_p95
 
     NODE_C_END = NODE_A_START + 4 * HEIGHT + MARGIN / 2
 
     ax1.xaxis.grid(True, which="both")
-    ax1.yaxis.grid(True, which="both")
+    ax1.yaxis.grid(True, which="major")
     ax1.set_axisbelow(True)
-    ax1.axhline(y=NODE_A_START - MARGIN / 2, color="k", linestyle="--")
+    ax1.axhline(y=NODE_A_START - MARGIN / 2, color="k", linestyle="-")
     for run in runs:
         if run[3].startswith("node-a"):
             offset = NODE_A_START
         else:
             print("Wrong machine", run[3])
             sys.exit(1)
-        # print(run)
         for core in run[5]:
             ax1.barh(
-                int(core) * HEIGHT + offset + 10,
+                (4 - int(core) - 1) * HEIGHT + offset + 10,
                 (run[1] - run[0]).total_seconds(),
                 left=(run[0] - min(start_times)).total_seconds(),
                 color=run[4],
                 height=HEIGHT_NO_OFFSET,
                 align="edge",
             )
-    ax1.barh(NODE_A_START + 10, MAX_LEN, left=0, color=c_dict["memcached"], height=HEIGHT_NO_OFFSET, align="edge")
+    ax1.barh(
+        3 * HEIGHT + NODE_A_START + 10,
+        MAX_LEN,
+        left=0,
+        color=c_dict["memcached"],
+        height=HEIGHT_NO_OFFSET,
+        align="edge",
+    )
 
     ax1.set_xlim(0, MAX_LEN + 5)
 
     yticks = list(np.arange(0, 2001, 250))
     ylabels = list(np.arange(0, 2.001, 0.25))
+    ax1.set_yticks(yticks, labels=ylabels, color=c_dict["p95 latency"], minor=False)
+
+    yticks = []
+    ylabels = []
+
     for i in range(4):
         yticks.append(NODE_A_START + (2 * i + 1) * HEIGHT / 2)
-        ylabels.append(i)
-    ax1.set_ylabel("p95 tail latency [ms]", color="royalblue", fontdict={"fontsize": 16})
-    ax1.set_yticks(yticks, labels=ylabels)
+        ylabels.append(4 - i - 1)
 
-    ax1.set_title(f"Temporary Title", fontdict={"fontsize": 18}, pad=0)
+    ax1.set_yticks(yticks, labels=ylabels, minor=True)
+
+    ax1.set_ylabel("P95 Latency (ms)", color=c_dict["p95 latency"], fontdict={"fontsize": 16})
 
     ax1.set_ylim(0, NODE_C_END)
 
@@ -178,19 +214,21 @@ for RUN in RUNS:
     ax1.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
     ax1.legend(
         loc="upper center",
-        bbox_to_anchor=(0.5, -0.08),
-        fancybox=True,
-        shadow=True,
-        ncol=5,
+        bbox_to_anchor=(0.5, 1.15),
+        fancybox=False,
+        shadow=False,
+        ncol=4,
         handles=legend_elements,
         prop={"size": 16},
     )
 
-    ax2.set_ylim([0, max_q * 1.25])
+    ax2.set_ylim([0, max_q * 1.215])
     yticks = list(np.arange(0, max_q + 1, 25000))
     ylabels = list(np.arange(0, max_q + 1, 25000))
-    ax2.set_yticks(yticks, ylabels)
-    ax2.set_ylabel("QPS", color="orange", fontdict={"fontsize": 16})
-    ax1.set_xlabel("Time since memcached started [s]", fontdict={"fontsize": 16})
+    ax2.set_yticks(yticks, ylabels, color=c_dict["QPS"])
+    ax2.set_ylabel("QPS", color=c_dict["QPS"], fontdict={"fontsize": 16})
+    ax1.set_xlabel("Time since first PARSEC benchmark started (sec)", fontdict={"fontsize": 16})
+
+    fig.subplots_adjust(top=0.85)
 
     plt.savefig(fname=f"{BASE_DIR}/int{INTERVAL}_run{RUN}/plot.pdf")
